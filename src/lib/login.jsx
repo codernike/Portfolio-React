3b6147ae18f3ee0641bfd1a96ccf20739d696072
codernike <108070610+codernike@users.noreply.github.com>
2022-08-01 17:54:49 +0530

    dev7

--- src/lib/login.jsx
+++ src/lib/login.jsx
@@ -5,7 +5,7 @@ export default function login() {
     <>
     <center>   
         <form action="" id="login-id">
-         <center><h1 class="" style={{fontSize:'25px'}}>Contact Us</h1></center> 
+         <center><h1 class="" style={{fontSize:'25px'}}>Login Us</h1></center> 
      <center><input type="text" placeholder="Enter Name " class="input-login"/><input type="Email" placeholder="Enter Email " class="input-login"/><input type=" Password" placeholder="Enter Password " class="input-login"/> <button class="btn" style={{marginTop: '11px',borderradius: '0px'}}>Login</button></center>
         </form></center>
     </>
