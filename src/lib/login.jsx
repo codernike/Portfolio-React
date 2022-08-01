d66bbdd9f4cd1f3c4a6ff6dec76fc6b18b098dbd
codernike <108070610+codernike@users.noreply.github.com>
2022-08-01 17:39:32 +0530

    dev5

---
+++ src/lib/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function login() {
+  return (
+    <>
+    <center>   
+        <form action="" id="login-id">
+         <center><h1 class="" style={{fontSize:'25px'}}>Contact Us</h1></center> 
+     <center><input type="text" placeholder="Enter Name " class="input-login"/><input type="Email" placeholder="Enter Email " class="input-login"/><input type=" Password" placeholder="Enter Password " class="input-login"/> <button class="btn" style={{marginTop: '11px',borderradius: '0px'}}>Login</button></center>
+        </form></center>
+    </>
+  )
+}
